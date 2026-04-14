b3cbcd2bae4927169085eedc34368aacceb15507
nicolastaen <nic.tapiae@duocuc.cl>
2026-04-14 13:50:49 -0400

    se cambio toda la configuracion del framework

--- splim/src/main/java/com/duoc/splim/SplimApplication.java
+++ splim/src/main/java/com/duoc/splim/SplimApplication.java
@@ -1,4 +1,4 @@
-package splim;
+package com.duoc.splim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- splim/src/test/java/com/duoc/splim/SplimApplicationTests.java
+++ splim/src/test/java/com/duoc/splim/SplimApplicationTests.java
@@ -1,4 +1,4 @@
-package splim;
+package com.duoc.splim;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
